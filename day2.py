5e52190cd2a13ae3e8fe6debf157c58610fa1d05
James Deal <j891319@protonmail.com>
2025-12-03 17:59:29 +0000

    day2 pt2

--- day2.py
+++ day2.py
@@ -19,4 +19,36 @@ def maxVolt():
             maxVolt += dig1*10 + dig2
     return maxVolt
 
-print(maxVolt())
+#print(maxVolt())
+
+# Part 2: choose exactly 12 batteries in each bank.
+
+def maxVolt2():
+    maxVolt = 0
+    with open('input2', 'r') as f:
+        for line in f:
+            possib = []
+            print("Bank :" + line)
+            for char in line:
+                if char and char != '\n':
+                    possib.append(int(char))
+
+            digs = []
+
+            # sliding window max
+            lptr = 0
+            rptr = len(possib)-11
+            while len(digs) < 12:
+                best = max(possib[lptr:rptr])
+                digs.append(best)
+                lptr = (possib[lptr:rptr].index(best)+lptr)+1
+                rptr += 1
+
+            print("result:")
+            print(digs)
+
+            maxVolt += int(''.join(map(str,digs)))
+    return maxVolt
+
+print(maxVolt2())
+
