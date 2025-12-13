45c10bd1476c60fb7aac50c95893f3b8a4b96449
James Deal <j891319@protonmail.com>
2025-12-13 10:01:32 +0000

    day4 p2 passes

--- day4.py
+++ day4.py
@@ -33,30 +33,39 @@ def freshCount():
 # Part 2: how many ingredient IDs are considered fresh according to the ID ranges?
 
 def freshCount2():
-    ranges = True
-    safe = set()
+    # Store ranges in a list, 
+    # if a new range touches or overlaps with an existing range, update it.
+    # if it doesn't touch or overlap, add a new range.
+    ranges = []
     count = 0
     with open('input4', 'r') as f:
         for line in f:
-            if ranges:
-                # building safe set
-                if line == '\n':
-                    ranges = False
-                else:
-                    start, end = line.split('-')
-                    safe.add(range(int(start),int(end)+1))
+            # building safe set
+            if line == '\n':
+                break
             else:
-                # check IDs
-                for safelist in safe:
-                    seen = set()
-                    for safeVal in safelist:
-                        if safeVal in seen:
-                            continue
-                        count += 1
-                        seen.add(safeVal)
-
-            print('.')
-
+                startEnd = list(map(int,line.split('-')))
+                start, end = startEnd[0], startEnd[1]
+                # check for existing range overlap
+                while True:
+                    merged = False
+                    for rangeR in ranges.copy():
+                        startR, endR = rangeR[0], rangeR[1]
+                        # we want the opposite of when they do not touch or overlap
+                        # no overlap is when end < startR or start > endR
+                        # the negation is:
+                        if not (start > endR) and not (end < startR):
+                            ranges.pop(ranges.index((startR,endR)))
+                            start = min(start, startR)
+                            end = max(end, endR)
+                            merged = True
+                            break
+                    if not merged:
+                        ranges.append((start,end))
+                        break
+    for range in ranges:
+        start, end = range[0], range[1]
+        count += end-start + 1
     return count
 
 print(freshCount2())
